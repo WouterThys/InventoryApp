db18319beace1f2e4e4e48e5879085040b75bf13
Wouter Thys <thys_wouter@hotmail.com>
2017-03-05 13:58:30 +0100

    initalised on windows

--- src/main/java/com/waldo/inventory/Utils/validators/NotEmptyValidator.java
+++ src/main/java/com/waldo/inventory/Utils/validators/NotEmptyValidator.java
@@ -1,7 +1,18 @@
 package com.waldo.inventory.Utils.validators;
 
+import com.waldo.inventory.Utils.Error;
 import com.waldo.inventory.Utils.ErrorProvider;
+
+import javax.swing.*;
 
 public class NotEmptyValidator extends ErrorProvider {
 
+    public NotEmptyValidator(JComponent c) {
+        super(c);
+    }
+
+    @Override
+    protected Error ErrorDefinition(JComponent c) {
+        return null;
+    }
 }
