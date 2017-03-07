9a4c0a3bbd8b61c539829dfe701e44c74d3c527d
WouterThys <thys_wouter@hotmail.com>
2017-03-07 19:08:22 +0100

    ItemCategories with static int types

--- src/main/java/com/waldo/inventory/Utils/statics/ItemProcuctTypes.java
+++
@@ -1,7 +0,0 @@
-package com.waldo.inventory.Utils.statics;
-
-/**
- * Created by waldo on 3/5/17.
- */
-public class ItemProcuctTypes {
-}
--- src/main/java/com/waldo/inventory/Utils/statics/ItemProductTypes.java
+++ src/main/java/com/waldo/inventory/Utils/statics/ItemProductTypes.java
@@ -1,4 +1,4 @@
 package com.waldo.inventory.Utils.statics;
 
-public class ItemTypes {
+public class ItemProductTypes {
 }
