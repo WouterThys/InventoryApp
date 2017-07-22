a827ca049a46a4331be052e712455973706059b9
WouterThys <thys_wouter@hotmail.com>
2017-07-22 12:20:43 +0200

    added migration files

---
+++ migration_script.sql
@@ -0,0 +1,16 @@
+-- ----------------------------------------------------------------------------
+-- MySQL Workbench Migration
+-- Migrated Schemata: inventory
+-- Source Schemata: inventory
+-- Created: Fri Jul 21 12:29:08 2017
+-- Workbench Version: 6.3.6
+-- ----------------------------------------------------------------------------
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------------------------------------------------------
+-- Schema inventory
+-- ----------------------------------------------------------------------------
+DROP SCHEMA IF EXISTS `inventory` ;
+CREATE SCHEMA IF NOT EXISTS `inventory` ;
+SET FOREIGN_KEY_CHECKS = 1;
