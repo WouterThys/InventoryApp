7612cb83492cf539e11f82d73d0778860624c6ba
Wouter <thys_wouter@hotmail.com>
2017-12-03 23:25:58 +0100

    changes

--- src/main/java/com/waldo/inventory/Main.java
+++ src/main/java/com/waldo/inventory/Main.java
@@ -79,11 +79,6 @@ public class Main {
                     UIDefaults defaults = super.getDefaults();
 
                     defaults.put("defaultFont", new Font(Font.SANS_SERIF, Font.PLAIN, 15));
-//                    defaults.put("TabbedPane:TabbedPaneTabArea[Disabled].backgroundPainter", null);
-//                    defaults.put("TabbedPane:TabbedPaneTabArea[Enabled+MouseOver].backgroundPainter", null);
-//                    defaults.put("TabbedPane:TabbedPaneTabArea[Enabled+Pressed].backgroundPainter", null);
-//                    defaults.put("TabbedPane:TabbedPaneTabArea[Enabled].backgroundPainter", null);
-
                     return defaults;
                 }
             });
@@ -91,63 +86,6 @@ public class Main {
             LOG.error("Error settings look and feel.", e);
         }
 
-
-//        UIManager.put("Table.selectionForeground", Color.YELLOW);
-//        try {
-//            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getNameText())) {
-//                    UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (Exception e) {
-//            // If Nimbus is not available, you can set the GUI to another look and feel.
-//        }
-        //setAllFonts(new FontUIResource("sansserif", Font.PLAIN, 15));
-
-//        SynthLookAndFeel lookAndFeel = new SynthLookAndFeel();
-//
-//        try {
-//            InputStream stream = Main.class.getResourceAsStream("/lookandfeel.xml");
-//            lookAndFeel.load(stream, Main.class);
-//            UIManager.setLookAndFeel(lookAndFeel);
-//        } catch (Exception e) {
-//            System.err.println("Couldn't get specified look and feel ("
-//                    + lookAndFeel
-//                    + "), for some reason.");
-//            System.err.println("Using the default look and feel.");
-//        }
-    }
-
-    private static void setAllFonts(javax.swing.plaf.FontUIResource f) {
-        java.util.Enumeration keys = UIManager.getDefaults().keys();
-        while (keys.hasMoreElements()) {
-            Object key = keys.nextElement();
-            Object value = UIManager.get(key);
-            if (value != null && value instanceof javax.swing.plaf.FontUIResource)
-                UIManager.put(key, f);
-        }
-    }
-
-    private static void updateLookAndFeel() {
-        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-            if ("Nimbus".equals(info.getName())) {
-                try {
-                    UIManager.setLookAndFeel(info.getClassName());
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-                    LOG.error("Error loading Nimbus look and feel", e);
-                }
-                UIManager.getLookAndFeelDefaults().put(
-                        "TabbedPane:TabbedPaneTabArea[Disabled].backgroundPainter", null);
-                UIManager.getLookAndFeelDefaults().put(
-                        "TabbedPane:TabbedPaneTabArea[Enabled+MouseOver].backgroundPainter", null);
-                UIManager.getLookAndFeelDefaults().put(
-                        "TabbedPane:TabbedPaneTabArea[Enabled+Pressed].backgroundPainter", null);
-                UIManager.getLookAndFeelDefaults().put(
-                        "TabbedPane:TabbedPaneTabArea[Enabled].backgroundPainter", null);
-                break;
-            }
-        }
     }
 
     public static void closeApplication(int status) {
